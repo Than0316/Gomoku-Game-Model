8abd01b56a2f280ffc7de6dfd20e8c1319e792a9
Tianyi Han <tina.h@Tianyidebijibendiannao.local>
2025-12-20 16:16:37 +0100

    Remove obsolete files

--- GOMOKU_config.py
+++
@@ -1,5 +0,0 @@
-class Config:
-    size = 15
-    points_limit = 20 # Limit for number of moves to return in heuristic search
-    inline_count = 4  # Used for "in-line" optimization (not fully implemented in basic version but kept for compatibility)
-    inline_distance = 5
